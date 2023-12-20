ade63a66aa98535ef00517566fa9e375c62444bb
Torremolinos <adrian_2054@hotmail.com>
2023-12-20 13:13:20 +0100

    cambios en la sesion

--- listado_taquillas.php
+++ listado_taquillas.php
@@ -1,15 +1,19 @@
 <?php
 require "connection.php";
 $conexion = conectarBD();
-$localidadSeleccionada = 'todas las localidades';
+
 session_start();
-if (isset($_GET['localidad'])) {
-    $_SESSION['localidad'] = $_GET['localidad'];
-} elseif (isset($_SESSION['localidad'])) {
-    echo "Última localidad seleccionada: " . $_SESSION['localidad'];
-} else {
-    $_SESSION['localidad'] = "";
+
+$localidadSeleccionada = '';
+if(isset($_SESSION['localidad'])) {
+    $localidadSeleccionada = $_SESSION['localidad'];
 }
+
+if(isset($_GET['localidad'])) {
+    $localidadSeleccionada = $_GET['localidad'];
+    $_SESSION['localidad'] = $localidadSeleccionada;
+}
+
 ?>
 <!DOCTYPE html>
 
@@ -22,12 +26,12 @@ if (isset($_GET['localidad'])) {
 
 <body>
     <form action="" method="get">
-        <select name="localidad">
-            <option value="">Todas las localidades</option>
-            <option value="Gijón">Gijón</option>
-            <option value="Oviedo">Oviedo</option>
-            <option value="Avilés">Avilés</option>
-        </select>
+    <select name="localidad">
+        <option value="">Todas las localidades</option>
+        <option value="Gijón" <?= $localidadSeleccionada == 'Gijón' ? 'selected' : ''; ?> >Gijón</option>
+        <option value="Oviedo" <?= $localidadSeleccionada == 'Oviedo' ? 'selected' : ''; ?>>Oviedo</option>
+        <option value="Avilés" <?= $localidadSeleccionada == 'Avilés' ? 'selected' : ''; ?>>Avilés</option>
+    </select>
         <input type="submit" value="Buscar">
     </form>
 
